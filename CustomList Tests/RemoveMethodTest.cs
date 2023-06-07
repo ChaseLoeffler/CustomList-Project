1015462532388c4d90e242ed1da9ef68f94d72e0
ChaseLoeffler <chaselo609@gmail.com>
2023-06-07 17:02:21 -0400

    Created RemoveMethodTest. Test checks that only the first instance of a object in the collection is removed all others remain.

--- CustomList Tests/RemoveMethodTest.cs
+++ CustomList Tests/RemoveMethodTest.cs
@@ -42,7 +42,7 @@ namespace CustomList_Tests
             Assert.AreEqual(2, myList.Count);
         }
         [TestMethod]
-        public void RemoveMethod_RemoveOneItem_RemainingItemsShiftDownIndex()
+        public void RemoveMethod_RemoveItemAtIndexFive_RemainingItemsAboveShiftDownIndex()
         {
             //Arrange
             CustomList<string> myList = new CustomList<string>();
@@ -61,5 +61,31 @@ namespace CustomList_Tests
             //Assert
             Assert.AreEqual("look", myList[5]);
         }
+        [TestMethod]
+        public void RemoveMethod_RemoveItemAtIndexFive_OtherInstancesOfTheItemRemainOnlyTheFirstIsRemoved()
+        {
+            //Arrange
+            CustomList<string> myList = new CustomList<string>();
+            //Act
+            myList.Add("hello");
+            myList.Add("world");
+            myList.Add("is");
+            myList.Add("it");
+            myList.Add("me");
+            myList.Add("you're");
+            myList.Add("looking");
+            myList.Add("for");
+            myList.Add("I");
+            myList.Add("can");
+            myList.Add("see");
+            myList.Add("it");
+            myList.Add("in");
+            myList.Add("eyes");
+            myList.Add("you're");
+            myList.Remove("you're");
+            //Assert
+            Assert.AreEqual("you're", myList[14]);
+        }
+
     }
 }
