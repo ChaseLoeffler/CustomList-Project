6224bebda38f307f6eeaedac152014027fe92537
ChaseLoeffler <chaselo609@gmail.com>
2023-06-07 16:33:24 -0400

    Created RemoveMethodTests. Created Two tests. Test one checks to see if list count decrements when Method is used. Test two Checks if Method returns true when item is successfully removed.

---
+++ CustomList Tests/RemoveMethodTest.cs
@@ -0,0 +1,33 @@
+﻿using CustomList;
+
+namespace CustomList_Tests
+{
+    [TestClass]
+    public class RemoveMethodTest
+    {
+        [TestMethod]
+        public void RemoveMethod_RemoveOneItem_CountDecrementsByOne()
+        {
+            //Arrange
+            CustomList<string> myList = new CustomList<string>();
+            //Act
+            myList.Add("Hello");
+            myList.Add("is");
+            myList.Remove("Hello");
+            //Assert
+            Assert.AreEqual(1, myList.Count);
+        }
+        [TestMethod]
+        public void RemoveMethod_RemoveOneItem_MethodReturnsTrueWhenItemIsRemoved()
+        {
+            //Arrange
+            CustomList<string> myList = new CustomList<string>();
+            //Act
+            myList.Add("Hello");
+            myList.Add("is");
+            myList.Remove("Hello");
+            //Assert
+            Assert.AreEqual(, myList.Count);
+        }
+    }
+}
