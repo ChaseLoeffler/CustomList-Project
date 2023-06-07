b196a10e9e2fdd9291fad1a78004d044bc4ffbf9
ChaseLoeffler <chaselo609@gmail.com>
2023-06-07 19:15:47 -0400

    Created To stringMethodTests class. Created first test. Test Method checks that a list of strings returns as expected.

---
+++ CustomList Tests/ToStringMethodTests.cs
@@ -0,0 +1,21 @@
+﻿using CustomList;
+
+namespace CustomList_Tests
+{
+    [TestClass]
+    public class ToStringMethodTests
+    {
+        [TestMethod]
+        public void ToStringMethod_CreatListOfStrings_ReturnsAListOfStrings()
+        {
+            //Arrange
+            CustomList<string> myList = new CustomList<string>();
+            //Act
+            myList.Add("Hello");
+            myList.Add("is");
+            string result = myList.ToString();
+            //Assert
+            Assert.AreEqual("Hello, is.", result);
+        }
+    }
+}
