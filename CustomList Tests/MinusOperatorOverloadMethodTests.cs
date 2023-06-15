75925ca257903cc3925ce1c25c2351db5752a777
ChaseLoeffler <chaselo609@gmail.com>
2023-06-15 10:54:45 -0400

    Created fourth test method in MinusOperator overload tests class. Test checks that only one instance of a vaule is subtracted from the first list.

--- CustomList Tests/MinusOperatorOverloadMethodTests.cs
+++ CustomList Tests/MinusOperatorOverloadMethodTests.cs
@@ -59,5 +59,50 @@ namespace CustomList_Tests
             //Assert
             Assert.AreEqual("4, 4", result);
         }
+        [TestMethod]
+        public void MinusOperatorOL_SubtractingListsFromEachOtherSecondListIsEmpty_ListIsUnchanged()
+        {
+            //Arrange
+            CustomList<int> oneList = new();
+            CustomList<int> twoList = new();
+            //Act
+            oneList.Add(1);
+            oneList.Add(2);
+            oneList.Add(3);
+            oneList.Add(1);
+            oneList.Add(2);
+            oneList.Add(3);
+            oneList.Add(1);
+            oneList.Add(2);
+
+            CustomList<int> newList = oneList - twoList;
+
+            string result = newList.ToString();
+
+            //Assert
+            Assert.AreEqual("1, 2, 3, 1, 2, 3, 1, 2", result);
+        }
+        [TestMethod]
+        public void MinusOperatorOL_SubtractingListsFromEachOther_OnlyTheFirstInstanceOfAValueIsRemoved()
+        {
+            //Arrange
+            CustomList<int> oneList = new();
+            CustomList<int> twoList = new();
+            //Act
+            oneList.Add(3);
+            oneList.Add(3);
+            oneList.Add(3);
+            
+            twoList.Add(1);
+            twoList.Add(2);
+            twoList.Add(3);
+           
+            CustomList<int> newList = oneList - twoList;
+
+            string result = newList.ToString();
+
+            //Assert
+            Assert.AreEqual("3, 3", result);
+        }
     }
 }
