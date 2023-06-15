fe66e7d55aa9efc65964bb7b4a3e5fda4cdcc84c
ChaseLoeffler <chaselo609@gmail.com>
2023-06-15 10:29:10 -0400

    Created new test class for the MinusOperator overload method. Created first test. Testing the result of the first list being larger then the second.

---
+++ CustomList Tests/MinusOperatorOverloadMethodTests.cs
@@ -0,0 +1,36 @@
+﻿using CustomList;
+
+namespace CustomList_Tests
+{
+    [TestClass]
+    public class MinusOperatorOverloadMethodTests
+    {
+        [TestMethod]
+        public void MinusOperatorOL_SubtractingListsFromEachOtherFirstListIsLonger_ItemsFromSecondListAreRemovedFromFirstList()
+        {
+            //Arrange
+            CustomList<int> oneList = new();
+            CustomList<int> twoList = new();
+            //Act
+            oneList.Add(1);
+            oneList.Add(2);
+            oneList.Add(3);
+            oneList.Add(1);
+            oneList.Add(2);
+            oneList.Add(3);
+            oneList.Add(1);
+            oneList.Add(2);
+
+            twoList.Add(3);
+            twoList.Add(4);
+            twoList.Add(3);
+            twoList.Add(4);
+            CustomList<int> newList = oneList - twoList;
+
+            string result = newList.ToString();
+
+            //Assert
+            Assert.AreEqual("1, 2, 1, 2, 1, 2", result);
+        }
+    }
+}
